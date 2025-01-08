3a6c575debcf4c3993402fa0d9f614a808a05793
Owen Tuleja <owentuleja@Owens-MacBook-Pro.local>
2025-01-08 20:24:46 +0100

    wip

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
+import M from "@/components/my-component";
 export default function Home() {
   return (
     <div>
-      <Button>button</Button>{" "}
+      {/* <Button>button</Button> */}
+      <M />
     </div>
   );
 }
---
+++ components/my-component.js
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>my component</div>;
+};
